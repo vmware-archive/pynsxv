2e208fc4dc1ea7f7ba268a60e1ad109fb6e8e466
yfauser <yfauser@vmware.com>
2016-05-29 15:42:32 +0200

    Modified LICENSE to include the right MIT License format Added NOTICE Files for Copyright details Modified the LICENSE and Copyright Information in all source Files

--- pynsxv/library/nsx_logical_switch.py
+++ pynsxv/library/nsx_logical_switch.py
@@ -1,20 +1,26 @@
+#!/usr/bin/env python
 # coding=utf-8
 #
-# Copyright © 2015 VMware, Inc. All Rights Reserved.
+# Copyright © 2015-2016 VMware, Inc. All Rights Reserved.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# Licensed under the X11 (MIT) (the “License”) set forth below;
 #
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions
-# of the Software.
+# you may not use this file except in compliance with the License. Unless required by applicable law or agreed to in
+# writing, software distributed under the License is distributed on an “AS IS” BASIS, without warranties or conditions
+# of any kind, EITHER EXPRESS OR IMPLIED. See the License for the specific language governing permissions and
+# limitations under the License. Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-# TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+#
+# "THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+# AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.”
 
 __author__ = 'Dimitri Desmidt, Emanuele Mazza, Yves Fauser'
 
